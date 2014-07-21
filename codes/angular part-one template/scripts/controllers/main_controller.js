2312dc6b448f747f135c9b8a587bd076b95ab28f
Channely <qijie29896@gmail.com>
2014-07-21 14:34:29 +0800

    debug controller code

--- codes/angular part-one template/scripts/controllers/main_controller.js
+++ codes/angular part-one template/scripts/controllers/main_controller.js
@@ -2,15 +2,10 @@
 
 angular.module('orderMasterApp')
     .controller('MainCtrl', function ($scope, $location) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
         $scope.people_number = 0;
         $scope.help_order = function () {
             $location.path('/order_details')
-        }
+        };
         $scope.get_order = function () {
             $location.path('/show_orders')
         }
--- codes/angular part-one template/scripts/controllers/order_details_controller.js
+++ codes/angular part-one template/scripts/controllers/order_details_controller.js
@@ -2,11 +2,6 @@
 
 angular.module('orderMasterApp')
     .controller('OrderDetailsCtrl', function ($scope, $location) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
         $scope.people_number = 0;
 
 
