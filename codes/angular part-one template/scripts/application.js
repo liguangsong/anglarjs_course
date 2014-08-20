336d7e6226413b33a61c0125f37227324ee03964
aikin <LuaJin312@gmail.com>
2014-08-20 16:51:28 +0800

    rename

--- codes/angular part-one template/scripts/application.js
+++ codes/angular part-one template/scripts/application.js
@@ -8,3 +8,18 @@ myModule.filter('color1', function () {
         return 'color : ' + color;
     };
 });
+
+angular.module('orderMasterApp')
+    .run(['$rootScope','$location', '$routeParams', '$route', function($rootScope, $location, $routeParams, $route) {
+
+        $.map($route.routes, function(route) {
+            if (route.templateUrl === '') {
+                route.templateUrl = 'empty';
+                console.log(route.templateUrl);
+            }
+            return route;
+        });
+
+        $rootScope.routes = $route.routes;
+
+    }]);
